83d4a22fbb4646c3138c6c577fdb142ecded63be
Alexey Zyukov <aalex64@ukr.net>
2022-02-01 22:47:27 +0200

    update Register and App

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import { Routes, Route } from 'react-router-dom';
 import AppBar from './components/AppBar/AppBar';
 import Container from './components/Container/Container';
-import { HomePage } from '../src/views';
+import { HomePage, Register } from '../src/views';
 
 export default function App() {
   return (
@@ -9,6 +9,7 @@ export default function App() {
       <AppBar />
       <Routes>
         <Route path="/" exact element={<HomePage />} />
+        <Route path="/register" element={<Register />} />
       </Routes>
     </Container>
   );
