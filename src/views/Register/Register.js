56590b9de8b1be14aac8b3b7765268bd925d15cf
Alexey Zyukov <aalex64@ukr.net>
2022-02-03 23:27:55 +0200

    update registration

--- src/views/Register/Register.js
+++ src/views/Register/Register.js
@@ -1,11 +1,44 @@
+import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import authOperations from '../../redux/auth/auth-operations';
+
 import s from './register.module.css';
 
 export default function RegisterView() {
-  // const dispatch = useDispatch();
-  // const [name, setName] = useState('');
-  // const [email, setEmail] = useState('');
-  // const [password, setPassword] = useState('');
+  const dispatch = useDispatch();
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
 
+  const handleChange = ({ target: { name, value } }) => {
+    switch (name) {
+      case 'name':
+        return setName(value);
+      case 'email':
+        return setEmail(value);
+      case 'password':
+        return setPassword(value);
+      default:
+        break;
+    }
+  };
+  const handleSubmit = e => {
+    e.preventDefault();
+    dispatch(
+      authOperations.register({
+        name,
+        email,
+        password,
+      }),
+    );
+    reset();
+  };
+
+  function reset() {
+    setName('');
+    setEmail('');
+    setPassword('');
+  }
   // const handleChange = ({ target: { name, value } }) => {
   //     switch (name) {
   //         case 'name':
@@ -29,23 +62,24 @@ export default function RegisterView() {
 
   return (
     <>
-      <h2 style={{ textAlign: 'center', marginTop: 20 }}>Create an account</h2>
+      <h2 style={{ textAlign: 'center', marginTop: 20 }}>
+        Register to create an account
+      </h2>
       <br />
-      <form
-        // onSubmit={handleSubmit}
-        className={s.form}
-        autoComplete="off"
-      >
+      <form onSubmit={handleSubmit} className={s.form} autoComplete="off">
         <label className={s.label}>
           Name
           <input
             type="text"
             name="name"
             placeholder="enter your name"
-            // value={name}
-            // onChange={handleChange}
-            className={s.input}
+            value={name}
+            onChange={handleChange}
+            required
             autoFocus="on"
+            pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+            title="Имя может состоять только из букв, апострофа, тире и пробелов. Например Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan и т. п."
+            className={s.input}
           />
         </label>
 
@@ -55,8 +89,9 @@ export default function RegisterView() {
             type="email"
             name="email"
             placeholder="enter e-mail"
-            // value={email}
-            // onChange={handleChange}
+            value={email}
+            onChange={handleChange}
+            required
             className={s.input}
           />
         </label>
@@ -67,8 +102,9 @@ export default function RegisterView() {
             type="password"
             name="password"
             placeholder="create password"
-            // value={password}
-            // onChange={handleChange}
+            value={password}
+            onChange={handleChange}
+            required
             className={s.input}
           />
         </label>
